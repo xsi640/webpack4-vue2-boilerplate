77df9a4fbf90433c74400c03977d459f7dd2ea0d
苏扬 <suyang@ekuaibao.com>
2019-04-30 14:39:49 +0800

    init

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,43 @@
+'use strict'
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const utils = require('./utils')
+const config = require('../config')
+const env = config.dev.env
+
+const HOST = 'localhost'
+const PORT = 8030
+
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    clientLogLevel: 'warning',
+    // hot: true,
+    contentBase: 'dist',
+    compress: true,
+    host: HOST,
+    port: PORT,
+    open: true,
+    overlay: { warnings: false, errors: true },
+    publicPath: '/',
+    quiet: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': env
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+})
